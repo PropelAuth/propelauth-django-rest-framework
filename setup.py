1c9e25fd4b1e00a458bd831d77db980a28e1f943
Andrew Israel <andrew@propelauth.com>
2022-08-17 16:39:24 -0700

    Add APIs for migrations

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="propelauth-django-rest-framework",
-    version="1.0.0",
+    version="1.0.1",
     description="A library for managing authentication in Django Rest Framework",
     long_description=README,
     long_description_content_type="text/markdown",
