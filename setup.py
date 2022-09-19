0bd2347a954cfc210148b3fd45c5e39b319f6366
Andrew Israel <andrew@propelauth.com>
2022-09-18 22:59:05 -0700

    Add API endpoints for configuring SAML and user management

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="propelauth-django-rest-framework",
-    version="1.0.1",
+    version="1.0.2",
     description="A library for managing authentication in Django Rest Framework",
     long_description=README,
     long_description_content_type="text/markdown",
