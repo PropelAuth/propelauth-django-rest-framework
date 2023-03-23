5945f3ea59706c9552df9fb5f3c9d64879082610
andrew-propelauth <89474256+andrew-propelauth@users.noreply.github.com>
2023-03-23 14:55:35 -0700

    Adds access token API + fix build issue (#2)
    * Fix build issue for some OS's based on pytest
    
    * Add access token API

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import find_packages, setup
 import pathlib
+import sys
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
+# See https://pytest-runner.readthedocs.io/en/latest/#conditional-requirement
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
 setup(
     name="propelauth-django-rest-framework",
-    version="2.0.1",
+    version="2.0.2",
     description="A library for managing authentication in Django Rest Framework",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -15,8 +20,8 @@ setup(
     author="PropelAuth",
     author_email="support@propelauth.com",
     license="MIT",
-    install_requires=["django", "djangorestframework", "propelauth-py", "requests"],
-    setup_requires=["pytest-runner"],
+    install_requires=["django", "djangorestframework", "propelauth-py==3.0.3", "requests"],
+    setup_requires=pytest_runner,
     tests_require=["pytest==4.4.1"],
     test_suite="tests",
 )
