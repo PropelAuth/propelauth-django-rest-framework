a94c63d8021141d985f145c4d814b939cf19734a
Paul Vatterott <pfvatterott@gmail.com>
2025-02-10 15:43:19 -0700

    Add ignore domain restrictions to create user api (#26)
    And bump versions

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setup(
     name="propelauth-django-rest-framework",
-    version="4.1.0",
+    version="4.1.1",
     description="A library for managing authentication in Django Rest Framework",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -24,7 +24,7 @@ setup(
     install_requires=[
         "django",
         "djangorestframework",
-        "propelauth-py==4.1.0",
+        "propelauth-py==4.1.1",
         "requests",
     ],
     setup_requires=pytest_runner,
