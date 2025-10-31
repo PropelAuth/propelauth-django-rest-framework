aef6b190a74e84bfba470bd8eb60965ca2916200
Paul Vatterott <pfvatterott@gmail.com>
2025-10-31 15:38:35 -0600

    new apis (#37)
    * new apis
    
    * version bumps
    
    * update requirements.txt

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setup(
     name="propelauth-django-rest-framework",
-    version="4.2.8",
+    version="4.2.9",
     description="A library for managing authentication in Django Rest Framework",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -24,7 +24,7 @@ setup(
     install_requires=[
         "django",
         "djangorestframework",
-        "propelauth-py==4.2.8",
+        "propelauth-py==4.2.9",
         "requests",
         "httpx>=0.28.1",
     ],
